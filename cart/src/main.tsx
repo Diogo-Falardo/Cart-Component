cbc71942eff4b5e398fb368fcc133d84bd0da8dc
Diogo Falardo <diogofalardo04@gmail.com>
2026-02-01 19:42:41 +0000

    feat: Product Visualizer

--- cart/src/main.tsx
+++ cart/src/main.tsx
@@ -1,13 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+// data
+import { FAKE_PRODUCTS } from "./types";
+import type { Product_Data } from "./types";
+// ui
 import Cart from "./cart";
-import CartAdd from "./cartAdd";
+import ProductVisualizer from "./ProductVisualizer";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CartAdd productCode="1" />
-
-    <Cart />
+    <main className="w-full min-h-screen flex flex-col justify-center items-center">
+      {/* products displayer */}
+      <div className="grid grid-cols-4 row-auto gap-2">
+        {FAKE_PRODUCTS.map(
+          ({ productName, productCode, productPrice }: Product_Data) => (
+            <ProductVisualizer
+              key={productCode}
+              productName={productName}
+              productPrice={productPrice}
+              productCode={productCode}
+            />
+          ),
+        )}
+      </div>
+      {/* cart */}
+    </main>
   </StrictMode>,
 );
